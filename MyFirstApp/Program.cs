2c79329b868f0c553789315c2f5ea46bd03c28da
aleks257 <afrolov@tktk.ee>
2022-09-10 18:17:41 +0300

    create new app

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,15 @@
+﻿// Comment to my code
+
+
+//rakemdus küsib kasutajalt sisestada tema nime
+//rakemdus tervitab kasutajat nimepidi
+
+Console.WriteLine("Enter you name"); //output
+//string - sõne
+string userName = Console.ReadLine(); //input
+
+Console.WriteLine("Hello" + ", " + userName + "!"); //output
+//string interplotation
+
+Console.WriteLine($"Hello, {userName} !"); //output
+
